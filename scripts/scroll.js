acf93f5619d0c8b4248896de7fbb2993adec5a50
Professsor Jose de Assis <josedeassisfilho@gmail.com>
2022-03-02 18:28:14 -0300

    site update

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,11 +3,11 @@
  * @author Professor José de Assis
  */
 
-document.querySelector('#topo').addEventListener('click', function (event) {
-    event.preventDefault() //ignorar o link
-    topoSite('header') //"invocar" a fução topoSite passando o elemento header(cabeçalho) como "alvo" 
-})
+document.querySelector("#topo").addEventListener("click", function (event) {
+  event.preventDefault(); //ignorar o link
+  topoSite("header"); //"invocar" a fução topoSite passando o elemento header(cabeçalho) como "alvo"
+});
 
 function topoSite(element) {
-    document.querySelector(element).scrollIntoView({ behavior: 'smooth' }) //smooth(suave) auto(direto)
+  document.querySelector(element).scrollIntoView({ behavior: "smooth" }); //smooth(suave) auto(direto)
 }
