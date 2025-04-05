ab9ead73fafbc3f886329c88e88668551e22fb60
Professor José de Assis <josedeassisfilho@gmail.com>
2025-04-04 21:50:45 -0300

    HubLink

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/**
-    Scripts do site Professor josé de Assis
-    @author Professor José de Assis
- */
-
-// Atualizar automaticamente o ano (footer - copyright)  
-let ano = document.getElementById("copyrightYear")
-let anoAtual = new Date().getFullYear()
-ano.textContent = anoAtual
---
+++ script.js
@@ -0,0 +1,1 @@
+console.log("Página de links carregada com sucesso!")
